3f0d63af136603747260046549c65183d4c9d08f
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2013-11-28 13:38:56 -0600

    Included art_illumina in generate_genomes script, can now generate reads derived from genomes

--- scripts/generate_genomes.pl
+++ scripts/generate_genomes.pl
@@ -8,16 +8,21 @@ use strict;
 use FindBin;
 use lib $FindBin::Bin;
 
+use File::Copy 'move';
 use PositionsTable;
 use Bio::SeqIO;
 use Bio::Seq::SeqFactory;
 
 my $usage =
-"$0 [reference.fasta] [variants_table.tsv] [out_dir]\n".
+"$0 [reference.fasta] [variants_table.tsv] [art_illumina_parameters] [out_dir]\n".
 "Parameters:\n".
 "\treference.fasta:  A reference genome in FASTA format\n".
 "\tvariants_table.tsv:  A tab-deliminited table listing the genomes and variants to insert\n".
+"\tart_illumina_parameters:  A quoted string containing the art_illumina parameters\n".
 "\tout_dir:  The directory to store all output files\n";
+
+my $art_illumina_bin = `which art_illumina 2>/dev/null`;
+chomp $art_illumina_bin;
 
 # reads all reference sequences into a table structured like
 # ref_id => ref_seq
@@ -40,8 +45,9 @@ sub read_reference_sequences
 ############
 ### MAIN ###
 ############
-die $usage if (@ARGV != 3);
-my ($ref_file,$variants_file,$out_dir) = @ARGV;
+die $usage if (@ARGV != 4);
+die "error: dependency 'art_illumina' not found on PATH, please install this software and try again" if ((not defined $art_illumina_bin) or not (-e $art_illumina_bin));
+my ($ref_file,$variants_file,$art_illumina_parameters,$out_dir) = @ARGV;
 
 die "reference.fasta is not defined\n$usage" if (not defined $ref_file);
 die "$ref_file does not exist\n$usage" if (not -e $ref_file);
@@ -49,6 +55,7 @@ die "variants_table.tsv is not defined\n$usage" if (not defined $variants_file);
 die "$variants_file does not exist\n$usage" if (not -e $variants_file);
 die "out_dir is not defind\n$usage" if (not defined $out_dir);
 die "$out_dir already exists\n$usage" if (-e $out_dir);
+die "art_illumina_parameters not defined\n$usage" if (not defined $art_illumina_parameters);
 
 my $genomes_out_dir = "$out_dir/genomes";
 
@@ -61,17 +68,26 @@ die "error: no genomes to generate defined in $variants_file" if (@genome_names 
 # read original reference sequences
 my $reference_table = read_reference_sequences($ref_file);
 
+print "Running $0 with parameters\n";
+print "reference_file=$ref_file\n";
+print "variants_table=$variants_file\n";
+print "art_illumina_parameters=$art_illumina_parameters\n";
+print "out_dir=$out_dir\n";
+print "\n";
+
 mkdir $out_dir;
 mkdir $genomes_out_dir;
 
 my $factory = Bio::Seq::SeqFactory->new;
 
-# mutate each new genome and write to a file
 foreach my $genome_name (@genome_names)
 {
 	next if ($genome_name eq 'Reference');
 
 	my $genome_file = "$genomes_out_dir/$genome_name.fasta";
+	my $fastq_file_prefix = "$out_dir/$genome_name";
+
+	# mutate each new genome, write to file
 	my $variant_entry = $variants_core_table->{$genome_name};
 	for my $chrom (keys %$variant_entry)
 	{
@@ -103,7 +119,13 @@ foreach my $genome_name (@genome_names)
 		$seq_writer->write_seq($generated_sequence);
 	}
 	print "wrote $genome_file\n";
-}
-print "\n";
 
-# generate reads
+	# generate reads
+	my $art_command = "$art_illumina_bin -i $ref_file $art_illumina_parameters -o $fastq_file_prefix 1> $out_dir/$genome_name.log 2>&1";
+	print "running '$art_command'\n";
+	system($art_command) == 0 or die "Could not execute '$art_command'\n";
+	move("$fastq_file_prefix.fq", "$fastq_file_prefix.fastq");
+	print "wrote $fastq_file_prefix.fastq\n";
+	print "\n";
+}
+
